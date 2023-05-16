13ec60f36bfd6a994c4152c3172aebf07c2f778e
cksdud419 <91289935+cksdud419@users.noreply.github.com>
2023-05-16 22:16:40 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -25,7 +25,7 @@ function preload(){
 
 function setup() {
   //createCanvas(640, 480);
-  createCanvas(390, 520);
+  createCanvas(387, 516);
   //createCanvas(320, 240);
   //createCanvas(240, 320);
   //createCanvas(320, 480);
